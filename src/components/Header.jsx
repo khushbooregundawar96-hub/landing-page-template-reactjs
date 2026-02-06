63d56042cc138443ab6e480f6a6411bc0678940e
Khushboo <khushbooregundawar96@gmail.com>
2026-02-06 03:04:06 -0800

    feat: Add initial Header and Navbar components for the landing page.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ import headerImg from '../assets/pexels-binyamin-mellish-186078.png'
 
 const Header = () => {
 
-    const CustomBox = styled(Box) (({ theme }) => ({
+    const CustomBox = styled(Box)(({ theme }) => ({
         minHeight: '80vh',
         display: 'flex',
         justifyContent: 'center',
@@ -14,7 +14,7 @@ const Header = () => {
         gap: theme.spacing(2),
         paddingTop: theme.spacing(10),
         // cor de fundo
-        backgroundColor: 'orange',
+        backgroundColor: 'red',
         [theme.breakpoints.down('md')]: {
             flexDirection: 'column',
             alignItems: 'center',
@@ -22,7 +22,7 @@ const Header = () => {
         }
     }));
 
-    const BoxText = styled(Box) (({ theme }) => ({
+    const BoxText = styled(Box)(({ theme }) => ({
         flex: '1',
         paddingLeft: theme.spacing(8),
         [theme.breakpoints.down('md')]: {
@@ -34,81 +34,81 @@ const Header = () => {
     }));
 
 
-  return  (
+    return (
         <CustomBox component='header'>
             {/*  Box text  */}
-            <BoxText 
-            component='section'
+            <BoxText
+                component='section'
             >
                 <Typography
-                variant='h2'
-                component= 'h1'
-                sx={{
-                    fontWeight: 700,
-                    color: '#fff',
-                }}
+                    variant='h2'
+                    component='h1'
+                    sx={{
+                        fontWeight: 700,
+                        color: '#fff',
+                    }}
                 >
                     We'll build house of your dream
                 </Typography>
 
                 <Typography
-                variant='p'
-                component='p'
-                sx={{
-                    py: 3,
-                    lineHeight: 1.6,
-                    color: '#fff',
-                }}
+                    variant='p'
+                    component='p'
+                    sx={{
+                        py: 3,
+                        lineHeight: 1.6,
+                        color: '#fff',
+                    }}
                 >
                     We have 9000 more review and our customers
                     trust on out property and quality products.
                 </Typography>
 
                 <Box>
-                    <Button 
-                    variant='contained'
-                    sx={{
-                        mr: 2,
-                        px: 4, 
-                        py: 1,
-                        fontSize: '0.9rem',
-                        textTransform: 'capitalize',
-                        borderRadius: 0,
-                        borderColor: '#14192d',
-                        color: '#fff',
-                        backgroundColor: '#14192d',
-                        "&&:hover": {
-                            backgroundColor: "#343a55"
-                        },
-                        "&&:focus": {
-                            backgroundColor: "#343a55"
-                        }
-                    }}
+                    <Button
+                        variant='contained'
+                        sx={{
+                            mr: 2,
+                            px: 4,
+                            py: 1,
+                            fontSize: '0.9rem',
+                            textTransform: 'capitalize',
+                            borderRadius: 0,
+                            borderColor: '#14192d',
+                            color: '#fff',
+                            backgroundColor: '#14192d',
+                            "&&:hover": {
+                                backgroundColor: "#343a55"
+                            },
+                            "&&:focus": {
+                                backgroundColor: "#343a55"
+                            }
+                        }}
                     >
                         buy now
                     </Button>
-                    <Button 
-                    component={Link} 
-                    to={'/about'}
-                    variant='outlined'
-                    sx={{
-                        px: 4, 
-                        py: 1,
-                        fontSize:'0.9rem',
-                        textTransform: 'capitalize',
-                        borderRadius: 0,
-                        color: '#fff',
-                        backgroundColor: 'transparent',
-                        borderColor: '#fff',
-                        "&&:hover": {
-                            color: '#343a55',
-                            borderColor: '#343a55',
-                        },
-                        "&&:focus": {
-                            color: '#343a55',
-                            borderColor: '#343a55',
-                        }
-                    }}
+                    <Button
+                        component={Link}
+                        to={'/about'}
+                        variant='outlined'
+                        sx={{
+                            px: 4,
+                            py: 1,
+                            fontSize: '0.9rem',
+                            textTransform: 'capitalize',
+                            borderRadius: 0,
+                            color: '#fff',
+                            backgroundColor: 'transparent',
+                            borderColor: '#fff',
+                            "&&:hover": {
+                                color: '#343a55',
+                                borderColor: '#343a55',
+                            },
+                            "&&:focus": {
+                                color: '#343a55',
+                                borderColor: '#343a55',
+                            }
+                        }}
                     >
                         explore
                     </Button>
@@ -116,24 +116,24 @@ const Header = () => {
             </BoxText>
 
             <Box sx={theme => ({
-                [theme.breakpoints.down('md')]:{
+                [theme.breakpoints.down('md')]: {
                     flex: '1',
                     paddingTop: '30px',
                     alignSelf: 'center',
                 },
-                [theme.breakpoints.up('md')]:{
+                [theme.breakpoints.up('md')]: {
                     flex: '2',
                     alignSelf: 'flex-end',
                 },
             })}
             >
                 <img
-                src={headerImg}
-                alt="headerImg"
-                style={{ 
-                    width: "100%", 
-                    marginBottom: -15,
-                }}
+                    src={headerImg}
+                    alt="headerImg"
+                    style={{
+                        width: "100%",
+                        marginBottom: -15,
+                    }}
                 />
             </Box>
 
